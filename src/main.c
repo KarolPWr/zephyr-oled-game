16a0995391a464061471ef9ed20a410dc84ef1ed
Karol P <karprzy7@gmail.com>
2020-10-01 22:57:39 +0200

    Display instance ready

--- src/main.c
+++ src/main.c
@@ -1,41 +1,90 @@
 /*
- * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2018 PHYTEC Messtechnik GmbH
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr.h>
 #include <device.h>
-#include <drivers/gpio.h>
+#include <display/cfb.h>
+#include <stdio.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#if defined(CONFIG_SSD16XX)
+#define DISPLAY_DRIVER		"SSD16XX"
+#endif
 
-#ifndef DT_ALIAS_LED0_GPIOS_FLAGS
-#define DT_ALIAS_LED0_GPIOS_FLAGS 0
+#if defined(CONFIG_SSD1306)
+#define DISPLAY_DRIVER		"SSD1306"
+#endif
+
+#ifndef DISPLAY_DRIVER
+#define DISPLAY_DRIVER		"DISPLAY"
 #endif
 
 void main(void)
 {
 	struct device *dev;
-	bool led_is_on = true;
-	int ret;
+	u16_t rows;
+	u8_t ppt;
+	u8_t font_width;
+	u8_t font_height;
 
-	dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	dev = device_get_binding(DISPLAY_DRIVER);
+
 	if (dev == NULL) {
+		printf("Device not found\n");
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, DT_ALIAS_LED0_GPIOS_PIN,
-				 GPIO_OUTPUT_ACTIVE
-				 | DT_ALIAS_LED0_GPIOS_FLAGS);
-	if (ret < 0) {
+	if (display_set_pixel_format(dev, PIXEL_FORMAT_MONO10) != 0) {
+		printf("Failed to set required pixel format\n");
 		return;
 	}
+
+	printf("initialized %s\n", DISPLAY_DRIVER);
+
+	if (cfb_framebuffer_init(dev)) {
+		printf("Framebuffer initialization failed!\n");
+		return;
+	}
+
+	cfb_framebuffer_clear(dev, true);
+
+	display_blanking_off(dev);
+
+	rows = cfb_get_display_parameter(dev, CFB_DISPLAY_ROWS);
+	ppt = cfb_get_display_parameter(dev, CFB_DISPLAY_PPT);
+
+	for (int idx = 0; idx < 42; idx++) {
+		if (cfb_get_font_size(dev, idx, &font_width, &font_height)) {
+			break;
+		}
+		cfb_framebuffer_set_font(dev, idx);
+		printf("font width %d, font height %d\n",
+		       font_width, font_height);
+	}
+
+	printf("x_res %d, y_res %d, ppt %d, rows %d, cols %d\n",
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_WIDTH),
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_HEIGH),
+	       ppt,
+	       rows,
+	       cfb_get_display_parameter(dev, CFB_DISPLAY_COLS));
 
 	while (1) {
-		gpio_pin_set(dev, DT_ALIAS_LED0_GPIOS_PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_sleep(SLEEP_TIME_MS);
+		for (int i = 0; i < rows; i++) {
+			cfb_framebuffer_clear(dev, false);
+			if (cfb_print(dev,
+				      "01234",
+				      0, ppt)) {
+				printf("Failed to print a string\n");
+				continue;
+			}
+
+			cfb_framebuffer_finalize(dev);
+#if defined(CONFIG_ARCH_POSIX)
+			k_sleep(K_MSEC(10));
+#endif
+		}
 	}
 }
